0ee9a3ecaa953ae8596cd0458acfa55c435a629f
Tyler McKenna <tmckenna1988@gmail.com>
2024-02-13 17:39:31 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FEFA_App.Models;
+
+namespace FEFA_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FEFA_App.Models.User> User { get; set; } = default!;
+    }
+}
